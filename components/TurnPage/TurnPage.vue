2ffc8c21e7446f9f0879609987bbe10a75d97b05
pauloguerraf <paulo@nidointeractive.com>
2020-08-26 15:12:39 -0500

    last update

---
+++ components/TurnPage/TurnPage.vue
@@ -0,0 +1,75 @@
+<script>
+import $ from 'jquery'
+import './js/turn.js'
+import { nanoid } from 'nanoid'
+
+export default {
+  name: 'TurnPage',
+  data() {
+    return {
+      nanoid: nanoid(),
+      componentKey: 0,
+    }
+  },
+  props: {
+    options: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  watch: {
+    defaultOptions: {
+      handler(val) {
+        this.forceRerender(val)
+      },
+      deep: true,
+    },
+  },
+  computed: {
+    uid() {
+      return `jopa-${this.nanoid}`
+    },
+    selector() {
+      return `div[data-uid=${this.uid}]`
+    },
+    defaultOptions() {
+      return {
+        when: {
+          turning: (event, page, pageObj) => {
+            this.currentPage = page
+          },
+          // triggered before turned
+          first: () => {
+            this.first()
+          },
+          // triggered before turned
+          last: () => {
+            this.last()
+          },
+        },
+        ...this.options,
+      }
+    },
+  },
+  mounted() {
+    $(this.selector).turn(this.defaultOptions)
+  },
+  methods: {
+    goTo(page) {
+      $(this.selector).turn('page', page)
+    },
+    first() {},
+    last() {},
+    forceRerender(val) {
+      this.nanoid = nanoid()
+      this.componentKey += 1
+      this.$nextTick(() => $(this.selector).turn(val))
+    },
+  },
+}
+</script>
+<template>
+  <div :key="componentKey" :data-uid="this.uid">
+    <slot> </slot>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flex justify-center items-center h-screen overflow-hidden">
+    <TurnPage class="w-1/2" :options="options">
+      <div v-for="resource in resources" v-bind:key="resource.public_id">
+        <img
+          id="turnp"
+          class="imagen"
+          :src="
+            'https://res.cloudinary.com/nidointeractive/image/upload/v1596560910' +
+            '/' +
+            resource.public_id +
+            '.' +
+            resource.format
+          "
+          draggable="false"
+          alt=""
+        />
+      </div>
+    </TurnPage>
+  </div>
+</template>
+<script>
+import axios from '~/plugins/axios'
+import $ from 'jquery'
+
+export default {
+  data() {
+    return {
+      options: {},
+    }
+  },
+  async asyncData() {
+    const { data } = await axios.get(
+      //'https://jsonplaceholder.typicode.com/posts'
+      'https://res.cloudinary.com/nidointeractive/image/list/v1596595869/comic1.json'
+    )
+    return { resources: data.resources }
+  },
+  mounted() {
+    this.options = {
+      display: 'double',
+      acceleration: true,
+      autoCenter: true,
+      height: document.getElementById('turnp').clientHeight,
+      page: 1,
+    }
+  },
+}
+</script>
+<style></style>
